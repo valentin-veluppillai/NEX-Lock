14a7dc32183e131e52578582723c2bf2f95364c4
valentin-veluppillai <valentinveluppillai@gmail.com>
2018-02-25 13:46:13 +0100

    merge this asap

--- code/basics/users/doc.md
+++ code/basics/users/doc.md
@@ -1,4 +1,14 @@
 # user system
+## v0.2 [21.1.18]
+managed to fix the system w/o too much troble, simply forgot to reset file
+pointer (userData) after counting the lines in the lines in the file. fixed
+that by resetting the file pointer.
+
+### added
+test scenario, you can now enter an "uid", and the system decides
+wheter to grant you access or not
+
+logfiles
 ## v0.1 [21.1.18]
 erste versuche an einem file-basiertem usersystem
 
--- code/basics/users/main.c
+++ code/basics/users/main.c
@@ -1,7 +1,8 @@
+//users
+//author: valentin veluppillai
 #include <stdio.h>
 #include <string.h>
 
-int dbg = 0;
 
 struct usr {
   char name[80];
@@ -10,43 +11,70 @@ struct usr {
   int maxLockCount; // set to -1 for infinte unlocks
 };
 
+int findUid(int, int, struct usr*);
+
 int main() {
 
   char name[80];
+  int index;
   int uid;
   int lockCount;
   int maxLockCount;
-  //printf("%i\n", dbg++);
-  FILE * userData = fopen("users.txt", "r");
-  //printf("%i\n", dbg++);
   int userCount = 0;
 
+  FILE* userData = fopen("users.txt", "r");
+  FILE* logData;
 
-  while(fscanf(userData, "%s %3i %3i %3i\n", name, &uid, &lockCount, &maxLockCount) != -1)
+  while(fscanf(userData, "%s%i%i%i", name, &uid, &lockCount, &maxLockCount) != -1)
     userCount++;
 
   struct usr users[userCount];
 
-  int count = 0;
-  while(fscanf(userData, "%s %3i %3i %3i\n", users[count].name, &users[count].uid, &users[count].lockCount, &users[count].maxLockCount) != -1)
-    count++;
-  //printf("%i\n", dbg++);
-  /*
-  strcpy(users[0].name, "mark");
-  users[0].uid = 14;
-  users[0].lockCount = 0;
-  users[0].maxLockCount = 2;
-  printf("%i\n", dbg++);
-  strcpy(users[1].name, "marc");
-  users[1].uid = 902;
-  users[1].lockCount = 34;
-  users[1].maxLockCount = -1;
-  */
+  fclose(userData);
+  userData = fopen("users.txt", "r");
 
-  printf("%i\n", dbg++);
-  for( int i = 0; i < userCount; i++){
-    //printf("%i\n", dbg++);
-    printf("%s %i %i %i\n", users[i].name, users[i].uid, users[i].lockCount, users[i].maxLockCount);
+  for(int i = 0; i < userCount; i++)
+    fscanf(userData, "%s%i%i%i", users[i].name, &users[i].uid, &users[i].lockCount, &users[i].maxLockCount);
+
+  fclose(userData);
+
+  for( int i = 0; i < userCount; i++)
+    printf("entry %i:\tname: %s\tuid: %i\tlockcount: %i\tmaxlockcount: %i\n", i, users[i].name, users[i].uid, users[i].lockCount, users[i].maxLockCount);
+
+  printf("enter uid: ");
+  scanf("%i", &uid);
+  index = findUid(uid, userCount, users);
+
+  logData = fopen("log.txt", "a");
+
+  if(index != -1){
+    if( (users[index].lockCount >= users[index].maxLockCount)&&(users[index].maxLockCount >= 0)){
+      printf("\nDENIED: out of unlocks\n");
+      fprintf(logData, "User %s [UID: %i] tried to unlock; ERR: OOU\t time: insert_time_here\n", users[index].name, users[index].uid);
+    }
+    else{
+      printf("\nGRANTED\n");
+      fprintf(logData, "User %s [UID: %i] unlocked\t ERR: - \t time: insert_time_here\n", users[index].name, users[index].uid);
+      users[index].lockCount++;
+    }
   }
+  else{
+    printf("\nDENIED: unknown user\n");
+    fprintf(logData, "unknown user [UID: %i] tried to unlock\t ERR: UU\t time: insert_time_here\n", uid);
+  }
+
+  userData = fopen("users.txt", "w");
+  for( int i = 0; i < userCount; i++){
+    fprintf(userData, "%s %i %i %i\n", users[i].name, users[i].uid, users[i].lockCount, users[i].maxLockCount);
+  }
+  fclose(userData);
   return 0;
+}
+
+int findUid(int uid, int len, struct usr* users){
+  for(int i = 0; i < len; i++){
+    if(uid == (users+i)->uid)
+      return i;
+  }
+  return -1;
 }
